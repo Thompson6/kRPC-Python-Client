f26f65056b8b3db73f9bbbaf7a88500fe2d80379
Quack <tyryderle@gmail.com>
2026-06-17 00:15:03 -0700

    Colors engines

--- FlaskApp.py
+++ FlaskApp.py
@@ -95,4 +95,4 @@ def broadcast_all_engines():
     emit('all_engines', engines, broadcast=True)
 
 if __name__ == "__main__":
-    socketio.run(app)
+    socketio.run(app, host='0.0.0.0', port=5000)
